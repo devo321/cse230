09e8f9ad9fc5d3eab389ee82cf116e9cb65ee058
devo321 <devo321@gmail.com>
2020-01-29 14:41:39 -0700

    adds main program code

--- assignment3/assignment3.s
+++ assignment3/assignment3.s
@@ -14,19 +14,39 @@
 
 .data
 
-num1: .word
-num2: .word
-num3: .word
-num4: .word
-message1: .asciiz "Enter a value:"
-message2: .asciiz "Enter another value:"
-message3: .asciiz "Enter one more value:"
-ans1: .asciiz "num3+num4="
-ans2: .asciiz "num4-num1="
-ans3: .asciiz "num3*num1="
-ans4: .asciiz "num1/num2="
-ans5: .asciiz "num3 mod num1="
-ans6: .asciiz "(num3 mod num4)+((num2 - 21 * num1) / 5)="
+#num1: .word
+#num2: .word
+#num3: .word
+#num4: .word
+message1: .asciiz "\nEnter a value: "
+message2: .asciiz "\nEnter another value: "
+message3: .asciiz "\nEnter one more value: "
+ans1: .asciiz "\nnum3+num4= "
+ans2: .asciiz "\nnum4-num1= "
+ans3: .asciiz "\nnum3*num1= "
+ans4: .asciiz "\nnum1/num2= "
+ans5: .asciiz "\nnum3 mod num1= "
+ans6: .asciiz "\n(num3 mod num4)+((num2 - 21 * num1) / 5)= "
+
+.text
+.globl main  #define a global function main
+
+main:
+
+    la $a0, message1
+    li $v0, 4
+    syscall
+
+    li $v0, 5
+    syscall
+    move $a0, $v0
+    li $v0, 1
+    syscall
+    
+
+
+    jr $ra
+
 
 
 
